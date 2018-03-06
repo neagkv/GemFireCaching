1f5fa0d737d5b850b23c8821cf7608f5177df1c4
Kevin Neag <neagkv@gmail.com>
2018-03-06 10:03:19 -0500

    new repo

---
+++ src/main/java/com/hello/GemFireCaching/GemFireCachingApplication.java
@@ -0,0 +1,12 @@
+package com.hello.GemFireCaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemFireCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemFireCachingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hello/GemFireCaching/QuoteResponse.java
@@ -0,0 +1,29 @@
+package com.hello.GemFireCaching;
+
+/**
+ * @author Kevin Neag
+ */
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class QuoteResponse {
+
+    @JsonProperty("value")
+    private Quote quote;
+
+    @JsonProperty("type")
+    private String status;
+
+    @Override
+    public String toString() {
+        return String.format("{ @type = %1$s, quote = '%2$s', status = %3$s }",
+                getClass().getName(), getQuote(), getStatus());
+    }
+
+
+}
